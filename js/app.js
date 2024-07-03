be49a3dd89e8545b4d7b309016a61168f212ff89
Ali82Aps <a.82aps@gmail.com>
2024-07-03 23:05:31 +0330

    library: swiper slider

---
+++ js/app.js
@@ -0,0 +1,10 @@
+var swiper = new Swiper(".mySwiper", {
+    slidePerView: 'auto',
+    spaceBetween: 40,
+    centeredSlide: true,
+    loop: true,
+    pagination: {
+      el: ".swiper-pagination",
+      dynamicBullets: true,
+    },
+  });
